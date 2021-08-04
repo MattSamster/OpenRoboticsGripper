adce416f18a4140c4a91ee4184318ee14fcfc99b
Chae <56273897+changnoo@users.noreply.github.com>
2021-08-03 17:27:07 -0700

    Create GP2Y0A51SK0F.cpp

---
+++ IR-dist-conversion/GP2Y0A51SK0F-Library/GP2Y0A51SK0F.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <GP2Y0A51SK0F.h>
+
+
+void setup(int irPin)
+{
+    _irPin = irPin;
+    pinMode(_irPin, INPUT);
+}
+
+void sort(int a[], int size)
+{
+    for(int i=0; i<(size-1); i++) {
+    bool flag = true;
+    for(int o=0; o<(size-(i+1)); o++) {
+        if(a[o] > a[o+1]) {
+            int t = a[o];
+            a[o] = a[o+1];
+            a[o+1] = t;
+            flag = false;
+        }
+    }
+    if (flag) break;
+  }
+}
+
+
+int distance_cm()
+{
+  
+  int ir_val[num_cm] = {};
+  int distanceCM;
+  float current;
+  int median;
+
+
+  for (int i=0; i< num_cm; i++){
+      // Read analog value
+      ir_val[i] = analogRead(_irPin);
+  }
+
+  // Get the approx median
+  #if USE_MEDOFMEDIANS
+      median = medianOfMedians(ir_val, num_cm);
+  #else
+      sort(ir_val, num_cm);
+      median = ir_val[num_cm/2];
+  #endif
+
+
+    // find an empirical and non-linear formula
+    // that describes the relationship between output voltage and distance
+    // extrapolate from the inverse stuff
+    distance = (analogRead(_irPin) - 0.0833)/4.88;
+    
+    return distance;
+}
+
