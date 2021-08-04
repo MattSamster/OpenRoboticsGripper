065fd0b331e5359a17e4f5a2d1150c67efccea54
Chae <56273897+changnoo@users.noreply.github.com>
2021-08-03 17:10:11 -0700

    Create GP2Y0A51SK0F.h

---
+++ IR-dist-conversion/GP2Y0A51SK0F-Library/GP2Y0A51SK0F.h
@@ -0,0 +1,26 @@
+#ifndef DistanceGP2Y0A21YK_h
+#define DistanceGP2Y0A21YK_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+  #include <pins_arduino.h>
+#endif
+
+
+class IR_GP2Y0A51SK0F
+{
+  public:
+
+    int getDistance();
+    IR_GP2Y0A51SK0F (int irPin, long sensorModel);
+    
+    int distance();
+
+  private:
+
+    void sort(int a[], int size);
+    
+    int _irPin;
+    };
